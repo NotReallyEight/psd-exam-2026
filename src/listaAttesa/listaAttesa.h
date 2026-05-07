dfab8fe45e9c6c4847565368f7e4903e74083851
raff0909 <raffaele.severino9@gmail.com>
2026-05-07 20:52:57 +0200

    controllo commenti2

--- src/listaAttesa/listaAttesa.h
+++ src/listaAttesa/listaAttesa.h
@@ -1,48 +1,59 @@
 
 #define NUM_FASCE 5
 
-// Puntatore alla struttura interna della coda
+
 
 typedef struct elementoCodaInterno *elementoCoda;
+// Puntatore alla struttura interna della coda
 
 
 
-// enqueue: inserisce uno studente in coda per una fascia oraria
 
 int enqueue(const char *matricola, int fasciaOraria);
+// enqueue: inserisce uno studente in coda per una fascia oraria
 
-// dequeue: rimuove e restituisce il primo studente in attesa per una fascia oraria
+
+
 
 elementoCoda dequeue(int fasciaOraria);
+// dequeue: rimuove e restituisce il primo studente in attesa per una fascia oraria
 
-//peek: restituisce il primo studente in attesa senza rimuoverlo
  
-elementoCoda peek(int fasciaOraria);
 
-// rimuoviDaCoda: rimuove uno studente specifico dalla coda
+
+elementoCoda peek(int fasciaOraria);
+//peek: restituisce il primo studente in attesa senza rimuoverlo
+
+
+
 
 int rimuoviDaCoda(const char *matricola, int fasciaOraria);
+// rimuoviDaCoda: rimuove uno studente specifico dalla coda
 
 
 
-// visualizzaCoda: stampa a schermo tutti gli studenti in attesa per una fascia oraria
  
 void visualizzaCoda(int fasciaOraria);
+// visualizzaCoda: stampa a schermo tutti gli studenti in attesa per una fascia oraria
 
-// visualizzaTutteLeCoda: stampa a schermo tutti gli studenti
+
+
 
 void visualizzaTutteLeCoda();
+// visualizzaTutteLeCoda: stampa a schermo tutti gli studenti
 
 
 
-//codaVuota: controlla se la coda e' vuota per una fascia
  
 int codaVuota(int fasciaOraria);
+//codaVuota: controlla se la coda e' vuota per una fascia
 
-//dimensioneCoda: restituisce il numero di studenti in attesa per una fascia oraria
+
 
 
 int dimensioneCoda(int fasciaOraria);
+//dimensioneCoda: restituisce il numero di studenti in attesa per una fascia oraria
+
 
 
 const char *getMatricolaCoda(elementoCoda e); // restituisce la matricola 
