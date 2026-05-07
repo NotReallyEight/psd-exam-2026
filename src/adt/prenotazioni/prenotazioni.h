8f2eb6f52314306e1321ab78358600a3dd8ab0f9
raff0909 <raffaele.severino9@gmail.com>
2026-05-07 20:59:49 +0200

    controllo get e modifica

--- src/adt/prenotazioni/prenotazioni.h
+++ src/adt/prenotazioni/prenotazioni.h
@@ -1,20 +1,36 @@
 
 typedef struct prenotazioneInterna * prenotazione;
 
+
+
 int creaPrenotazione(const char*matricola, const char*data, int fasciaOraria, int posto);
 //creaPrenotazione: alloca e inserisce una nuova prenotazione
+
+
+
 
 int annullaPrenotazione(const char*matricola, const char*data, int fasciaOraria);
 //annullaPrenotazione: rimuove una prenotazione GIA esistente
 
+
+
+
 prenotazione cercaPrenotazione(const char *matricola, const char *data,int fasciaOraria);
 //cercaPrenotazione: cerca una prenotazione per matricola e data
+
+
+
 
 void visualizzaPrenotazioni();
 //visualizzaPrenotazioni: stampa tutte le prenotazioni attive
 
+
+
+
 void visualizzaPrenotazioniPerFascia(int fasciaOraria);
 //visualizzaPrenotazioniPerFascia: stampa le prenotazioni
+
+
 
 
 /*
@@ -24,7 +40,10 @@ senza mai accedere direttamente alla struct
 
 rispettando quindi l'information hidig
 */
-const char *matricolaPrenotazione(prenotazione p); //restituisce la matricola
-const char *dataPrenotazione(prenotazione p);//restituisce la data
-int fasciaOraria(prenotazione p);//restituisce la fascia oraria
-int postoAssegnato(prenotazione p);//restituisce il posto assegnato
+
+
+
+const char *getMatricolaPrenotazione(prenotazione p); //restituisce la matricola
+const char *getDataPrenotazione(prenotazione p);//restituisce la data
+int getFasciaOraria(prenotazione p);//restituisce la fascia oraria
+int getPostoAssegnato(prenotazione p);//restituisce il posto assegnato
